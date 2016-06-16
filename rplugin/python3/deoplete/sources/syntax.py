df0261f2ce689754b1d2de7cbb87977b7d5d4985
Shougo Matsushita <Shougo.Matsu@gmail.com>
2016-06-17 07:31:15 +0900

    Improve syntax source cache feature

--- rplugin/python3/deoplete/sources/syntax.py
+++ rplugin/python3/deoplete/sources/syntax.py
@@ -33,9 +33,13 @@ class Source(Base):
 
         self.name = 'syntax'
         self.mark = '[S]'
+        self.__include_files = {}
         self.vim.call('necosyntax#initialize')
 
-    def gather_candidates(self, context):
-        return [{ 'word': x } for x in
-                self.vim.call('necosyntax#gather_candidates')]
+    def on_event(self, context):
+        self.__include_files[context['filetype']] = [
+            { 'word': x } for x in
+            self.vim.call('necosyntax#gather_candidates')]
 
+    def gather_candidates(self, context):
+        return self.__include_files.get(context['filetype'], [])
